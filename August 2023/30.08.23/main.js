d12b686824048bd3f36f4699e0029588c38e071d
nirglus <nirgluskin@gmail.com>
2023-08-30 10:53:07 +0300

    complete task 2

--- August 2023/30.08.23/main.js
+++ August 2023/30.08.23/main.js
@@ -1,5 +1,8 @@
 //Task 1 
-
 var myDiv = document.getElementById("div");
 myDiv.innerHTML = "<h1>first dom app</h1>";
 console.log(myDiv.innerText);
+
+//Task 2
+var emptyPar = document.getElementById("par");
+emptyPar.innerText = "my dom app";
