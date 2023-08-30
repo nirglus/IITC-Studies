4852365784c1c2baf337a58f40cf060b6bfa9f71
nirglus <nirgluskin@gmail.com>
2023-08-30 11:34:43 +0300

    complete task 3

--- August 2023/30.08.23/main.js
+++ August 2023/30.08.23/main.js
@@ -6,3 +6,14 @@ console.log(myDiv.innerText);
 //Task 2
 var emptyPar = document.getElementById("par");
 emptyPar.innerText = "my dom app";
+
+//Task 3
+var myHeadings = document.getElementsByClassName("headings");
+heading1.innerText = "Hi";
+var heading2 = document.getElementById("secondHeading");
+heading2.innerText = "My";
+var heading3 = document.getElementById("thirdHeading");
+heading3.innerText = "Name is";
+var heading4 = document.getElementById("forthHeading");
+heading4.innerText = "What";
+console.log(heading1, heading2, heading3, heading4);
