bef570f878fda2318d06d242c78da2a90f56561b
nirglus <nirgluskin@gmail.com>
2023-08-31 20:42:30 +0300

    complete 11.1

--- August 2023/31.08.23/Task 11/task11.js
+++ August 2023/31.08.23/Task 11/task11.js
@@ -1,11 +1,36 @@
-var userName = prompt("Enter your name:");
-var birthDate = prompt("Enter your birth date: (in this format 00/00/1900");
-var birthYear = birthDate.substring(5, birthDate.length);
-if(birthYear < 2005){
-    document.body.innerHTML += `<h1 id="welcome">Welcome <span>${userName}</h1>`;
-    document.getElementById("welcome").style = "color: blue";
-}else{
-    document.body.innerHTML += `<h1 id="welcome">YOU ARE NOT ALLOWED TO BE HERE!</h1>
-    <img src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExb3psOGJheGdveWo0a3NiM3hwaHQ1MGphZGF6aDNmeGZmanZpbGNlYyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/3Hxy8wKadIdMjRXc9k/giphy.gif">`;
-    document.getElementById("welcome").style = "color: red";
+function checkAccess(){
+    var userName = prompt("Enter your name:");
+    var birthDate = prompt("Enter your birth date: (in this format 00/00/1900");
+    var birthYear = birthDate.substring(6, birthDate.length);
+    console.log(birthYear);
+    if(birthYear < 2005){
+        document.body.innerHTML += `<h1 id="welcome" onmouseover = "colorChangeHov()">Welcome <span>${userName}</h1>`;
+        document.getElementById("welcome").style = "color: blue";
+        document.body.innerHTML += `<button id="printNameList" onclick = "overAge()">Press to print</button>`
+    }else{
+        document.body.innerHTML += `<h1 id="welcome" onmouseover = "colorChangeHov()">YOU ARE NOT ALLOWED TO BE HERE!</h1>
+        <img src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExb3psOGJheGdveWo0a3NiM3hwaHQ1MGphZGF6aDNmeGZmanZpbGNlYyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/3Hxy8wKadIdMjRXc9k/giphy.gif">`;
+        document.getElementById("welcome").style = "color: red";
+        setTimeout(function (){
+            document.location.reload();
+          }, 3000);
+    }
+}
+checkAccess();
+
+
+function overAge(){    
+    var lName = prompt("Enter last name:");
+    var fNamesArr = [];
+    document.body.innerHTML += `<ol id="nameList"></ol>`
+    for(let i = 0; i < 4; i++){
+        var fName = prompt("Enter first name:");
+        fNamesArr.push(fName);
+        document.getElementById("nameList").innerHTML += `<li>${fName} ${lName}</li`
+    }
+}
+
+
+function colorChangeHov(){
+    document.getElementById("welcome").style = `color: green`;
 }
