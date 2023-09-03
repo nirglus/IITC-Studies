0bb57d2ec284a3c913370278494ebd34037d1061
nirglus <nirgluskin@gmail.com>
2023-09-03 15:39:50 +0300

    complete slider task 1

---
+++ August 2023/31.08.23/slider task/slider.js
@@ -0,0 +1,9 @@
+var imgArr = [
+    "https://media.formula1.com/image/upload/content/dam/fom-website/sutton/2022/Italy/Sunday/1422823415.jpg.transform/9col-retina/image.jpg",
+    "https://media.formula1.com/image/upload/content/dam/fom-website/manual/Misc/2022manual/GettyImages-1437759309.jpg.transform/9col-retina/image.jpg",
+    "https://cdn-6.motorsport.com/images/amp/Yv8jllv0/s1200/lando-norris-mclaren-mcl60-lea.webp",
+    "https://media.formula1.com/image/upload/content/dam/fom-website/sutton/2022/Bahrain/Sunday/1386703905.jpg.transform/9col-retina/image.jpg",
+    "https://media.formula1.com/image/upload/content/dam/fom-website/manual/Misc/2022manual/2022Races/EmiliaRomagnaGP/Saturday/imola-sprint-start.jpg.transform/9col-retina/image.jpg",
+    "https://media.contentapi.ea.com/content/dam/ea/f1/f1-23/common/articles/patch-note-v109/pj-f123-bel-w01-rus.jpg.adapt.1456w.jpg"
+
+];
