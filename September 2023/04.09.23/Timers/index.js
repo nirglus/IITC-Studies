74621c55c5ccf090822f436a9f1fdcef8763b77c
nirglus <nirgluskin@gmail.com>
2023-09-04 15:09:11 +0300

    complete task 4

--- September 2023/04.09.23/Timers/index.js
+++ September 2023/04.09.23/Timers/index.js
@@ -1,17 +1,17 @@
 //Task 1
 function printName(){
-    document.body.innerHTML =`
+    document.body.innerHTML +=`
     <h1>Nir</h1>`
 }
 setTimeout(printName, 7000);
 
 //Task 2
 function printHello(){
-    document.body.innerHTML = `
+    document.body.innerHTML += `
     <h1>Hello</h1>`
 }
 setTimeout(printHello, 5000);
-document.body.innerHTML = `
+document.body.innerHTML += `
 <h1>Bye</h1>`;
 
 //Task 3
@@ -20,6 +20,17 @@ function printTime(){
     var hours = date.getHours();
     var min = date.getMinutes();
     var sec = date.getSeconds();
-    document.body.innerHTML = `${hours},${min},${sec}`;
+    document.body.innerHTML += `${hours},${min},${sec}`;
 }
 setTimeout(printTime, 90000);
+
+//Task 4
+document.body.innerHTML += `
+<button onclick="cancelPrint()">Cancel</button>`;
+function printLove(){
+    document.body.innerHTML += `I love timers`
+}
+var cancel = setTimeout(printLove, 10000);
+function cancelPrint(){
+    clearTimeout(cancel);
+}
