22f9e1e9b919d1768f1026f5d59fb9b74e5280ee
nirglus <nirgluskin@gmail.com>
2023-09-04 14:53:24 +0300

    complete task 1

---
+++ September 2023/04.09.23/Timers/index.js
@@ -0,0 +1,6 @@
+//Task 1
+function printName(){
+    document.body.innerHTML += `
+    <h1>Nir</h1>`
+}
+setTimeout(printName, 7000);
--- September 2023/04.09.23/index.js
+++ September 2023/04.09.23/index.js
@@ -1,5 +1,5 @@
 //Task 1
-document.body.innerHTML += `
+document.body.innerHTML = `
 <h1>Hello world</h1>`
 document.getElementsByTagName("h1")[0].addEventListener("mouseover", function(){
     alert("Welcome");
