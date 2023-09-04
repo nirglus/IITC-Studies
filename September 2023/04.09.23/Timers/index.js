dcfd587a141b656c7f0dbef41c0e6c177256337c
nirglus <nirgluskin@gmail.com>
2023-09-04 14:59:17 +0300

    complete task 2

--- September 2023/04.09.23/Timers/index.js
+++ September 2023/04.09.23/Timers/index.js
@@ -1,6 +1,15 @@
 //Task 1
 function printName(){
-    document.body.innerHTML += `
+    document.body.innerHTML =`
     <h1>Nir</h1>`
 }
 setTimeout(printName, 7000);
+
+//Task 2
+function printHello(){
+    document.body.innerHTML = `
+    <h1>Hello</h1>`
+}
+setTimeout(printHello, 5000);
+document.body.innerHTML = `
+<h1>Bye</h1>`;
