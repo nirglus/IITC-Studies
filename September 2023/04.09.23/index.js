e6cf29b67a0cafdec7e21b881f60e207c127ea96
nirglus <nirgluskin@gmail.com>
2023-09-04 12:21:25 +0300

    complete task 2

--- September 2023/04.09.23/index.js
+++ September 2023/04.09.23/index.js
@@ -4,3 +4,15 @@ document.body.innerHTML += `
 document.getElementsByTagName("h1")[0].addEventListener("mouseover", function(){
     alert("Welcome");
 });
+
+//Task 2
+document.body.innerHTML += `
+<div id="timeDiv" style = "height: 200px; width: 200px;"></div>`
+var date = new Date();
+var hours = date.getHours();
+var min = date.getMinutes();
+var sec = date.getSeconds();
+var currentTime = document.getElementById("timeDiv");
+currentTime.addEventListener("mouseover", function(){
+    currentTime.innerHTML = `${hours}:${min}:${sec}`;
+});
