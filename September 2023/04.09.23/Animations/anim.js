057a002f24b29b7f5e740566b34119fa31611e25
nirglus <nirgluskin@gmail.com>
2023-09-04 16:30:34 +0300

    start task 1

--- September 2023/04.09.23/Animations/anim.js
+++ September 2023/04.09.23/Animations/anim.js
@@ -1,32 +1,40 @@
-var intervalId = setInterval(moveRight,10);
+// var intervalId = setInterval(moveRight,10);
+// var counter = 0;
+// function moveRight(){
+//     box.style.left = `${++counter}px`
+//     if(counter==150){
+//         clearInterval(intervalId);
+//         counter = 0;
+//         intervalId = setInterval(moveDown,10);
+//     }
+// }
+// function moveDown(){
+//     box.style.top = `${++counter}px`;
+//     if(counter==150){
+//         clearInterval(intervalId);
+//         intervalId = setInterval(moveLeft,10);
+//     }
+// }
+// function moveLeft(){
+//     box.style.left = `${--counter}px`;
+//     if(counter==0){
+//         clearInterval(intervalId);
+//         intervalId = setInterval(moveUp,10);
+//         counter = 150;
+//     }
+// }
+// function moveUp(){
+//     box.style.top = `${--counter}px`;
+//     if(counter==0){
+//         clearInterval(intervalId);
+//         intervalId = setInterval(moveRight,10);
+//     }
+// }
+
+//Task 1
+document.body.innerHTML += `<div id="task1" style="width:30vw; height:20vh; color="red"></div>`
 var counter = 0;
-function moveRight(){
-    box.style.left = `${++counter}px`
-    if(counter==150){
-        clearInterval(intervalId);
-        counter = 0;
-        intervalId = setInterval(moveDown,10);
-    }
-}
-function moveDown(){
-    box.style.top = `${++counter}px`;
-    if(counter==150){
-        clearInterval(intervalId);
-        intervalId = setInterval(moveLeft,10);
-    }
-}
-function moveLeft(){
-    box.style.left = `${--counter}px`;
-    if(counter==0){
-        clearInterval(intervalId);
-        intervalId = setInterval(moveUp,10);
-        counter = 150;
-    }
-}
-function moveUp(){
-    box.style.top = `${--counter}px`;
-    if(counter==0){
-        clearInterval(intervalId);
-        intervalId = setInterval(moveRight,10);
-    }
+var intervalId = 
+function getHigher(){
+    
 }
