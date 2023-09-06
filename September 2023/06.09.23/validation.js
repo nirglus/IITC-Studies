d045caa3e5d930a1e7c98e3b500fa86169ccba81
nirglus <nirgluskin@gmail.com>
2023-09-06 12:26:21 +0300

    complete task 6.1

--- September 2023/06.09.23/validation.js
+++ September 2023/06.09.23/validation.js
@@ -1,11 +1,14 @@
-// //Task 1
-// function validate(){
-//     if(num1.value == num2.value){
-//         return true;
-//     }else{
-//         return false;
-//     }
-// }
+//Task 1
+function validate(){
+    if(num1.value == num2.value){
+        return true;
+    }else{
+        num1.style.border = `solid 2px red`;
+        num2.style.border = `solid 2px red`;
+        document.body.innerHTML += `<span style="color:red">The numbers are not even</span>`
+        return false;
+    }
+}
 
 // //Task 2
 // function whosBigger(){
@@ -34,11 +37,13 @@
 //     }
 // }
 
-//Task 5
-function validateFirstChar(){
-    if(txt1.value[0] == txt2.value[0]){
-        return true;
-    }else{
-        return false;
-    }
-}
+// //Task 5
+// function validateFirstChar(){
+//     if(txt1.value[0] == txt2.value[0]){
+//         return true;
+//     }else{
+//         return false;
+//     }
+// }
+
+//Task 6
