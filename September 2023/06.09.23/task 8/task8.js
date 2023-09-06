9b6cd2c8c7a633bede274a6389d07340053fd3d3
nirglus <nirgluskin@gmail.com>
2023-09-06 16:46:50 +0300

    assign validateForm function to form

--- September 2023/06.09.23/task 8/task8.js
+++ September 2023/06.09.23/task 8/task8.js
@@ -1,12 +1,15 @@
 function validateForm(){
     var isStartUppercase = firstName.value[0] == firstName.value[0].toUpperCase();
     var isBeneath20Char = lastName.value.length < 20;
-    var workerBirthYear = +workerAge.substring(0,4);
+    var workerBirthYear = +workerAge.value.substring(0,4);
     var currentYear = new Date().getFullYear;
     var isOver16Under65 = currentYear - workerBirthYear > 16 && currentYear - workerBirthYear < 65;
     var isEndOnComIl = workerMail.value.lastIndexOf(".com") == workerMail.value.length - 4 ||
     workerMail.value.lastIndexOf("co.il") == workerMail.value.length - 5;
-    var validatePhone = workersPhone.value[0] == 0 && workersPhone.value.length == 10;
-    
-
+    var validatePhone = workerPhone.value[0] == 0 && workerPhone.value.length == 10;
+    if(isStartUppercase && isBeneath20Char && isOver16Under65 && isEndOnComIl && validatePhone){
+        return true;
+    }else{
+        return false;
+    }
 }
