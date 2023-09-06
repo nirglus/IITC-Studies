fd02d881eb14e99d69a50591222b70db55dd849d
nirglus <nirgluskin@gmail.com>
2023-09-06 12:18:03 +0300

    complete task 4

--- September 2023/06.09.23/validation.js
+++ September 2023/06.09.23/validation.js
@@ -7,20 +7,29 @@
 //     }
 // }
 
-//Task 2
-function whosBigger(){
-    if(num1.value > num2.value){
-        return true;
-    }else{
-        return false;
-    }
-}
+// //Task 2
+// function whosBigger(){
+//     if(num1.value > num2.value){
+//         return true;
+//     }else{
+//         return false;
+//     }
+// }
 
-//Task 3
-function validateStr(){
-    if(txt1.value == txt2.value){
+// //Task 3
+// function validateStr(){
+//     if(txt1.value == txt2.value){
+//         return true;
+//     }else{
+//         return false;
+//     }
+// }
+
+//Task 4
+function validateLength(){
+    if(txt1.value.length == txt2.value.length){
         return true;
-    }else{
+    }else {
         return false;
     }
 }
