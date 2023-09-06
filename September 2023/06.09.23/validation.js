2f62325e2f340ab070270c7818e3286cdb7b5599
nirglus <nirgluskin@gmail.com>
2023-09-06 13:17:32 +0300

    complete task 6.4

--- September 2023/06.09.23/validation.js
+++ September 2023/06.09.23/validation.js
@@ -10,37 +10,40 @@
 //     }
 // }
 
-//Task 2
-function whosBigger(){
-    if(num1.value > num2.value){
-        return true;
-    }else{
-        num1.style.border = `solid 2px red`;
-        document.body.innerHTML += `<span style="color:red">Number 1 is smaller than Number 2</span>`
-        return false;
-    }
-}
-
-//Task 3
-function validateStr(){
-    if(txt1.value == txt2.value){
-        return true;
-    }else{
-        txt1.style.border = `solid 2px red`;
-        txt2.style.border = `solid 2px red`;
-        document.body.innerHTML += `<span style="color:red">The strings are not the same</span>`
-        return false;
-    }
-}
-
-// //Task 4
-// function validateLength(){
-//     if(txt1.value.length == txt2.value.length){
+// //Task 2
+// function whosBigger(){
+//     if(num1.value > num2.value){
 //         return true;
-//     }else {
+//     }else{
+//         num1.style.border = `solid 2px red`;
+//         document.body.innerHTML += `<span style="color:red">Number 1 is smaller than Number 2</span>`
 //         return false;
 //     }
 // }
+
+// //Task 3
+// function validateStr(){
+//     if(txt1.value == txt2.value){
+//         return true;
+//     }else{
+//         txt1.style.border = `solid 2px red`;
+//         txt2.style.border = `solid 2px red`;
+//         document.body.innerHTML += `<span style="color:red">The strings are not the same</span>`
+//         return false;
+//     }
+// }
+
+//Task 4
+function validateLength(){
+    if(txt1.value.length == txt2.value.length){
+        return true;
+    }else {
+        txt1.style.border = `solid 2px red`;
+        txt2.style.border = `solid 2px red`;
+        document.body.innerHTML += `<span style="color:red">The strings are not the same length</span>`
+        return false;
+    }
+}
 
 // //Task 5
 // function validateFirstChar(){
