4ef0faa39df1d22ae271f0e5f9c9f35de857ea76
nirglus <nirgluskin@gmail.com>
2023-09-06 15:54:23 +0300

    add another solution for task 7

--- September 2023/06.09.23/validation.js
+++ September 2023/06.09.23/validation.js
@@ -57,46 +57,77 @@
 //     }
 // }
 
-//Task 7
-function validateUsername(){
-    if(userName.value.length < 10){
-        userName.style.border = `solid 2px red`;
-        document.body.innerHTML += `<p style="color:red">The username must be longer than 10 characters</p>`;
-        return false;
-    }
-    return true;
-}
-function validateEmail(){
-    if(userMail.value.lastIndexOf(".com") != userMail.value.length - 4){
-        userMail.style.border = `solid 2px red`;
-        document.body.innerHTML += `<p style="color:red">Email must end with ".com"</p>`;
-        return false;
-    }
-    return true;
-}
+// //Task 7
+// function validateUsername(){
+//     if(userName.value.length < 10){
+//         userName.style.border = `solid 2px red`;
+//         document.body.innerHTML += `<p style="color:red">The username must be longer than 10 characters</p>`;
+//         return false;
+//     }
+//     return true;
+// }
+// function validateEmail(){
+//     if(userMail.value.lastIndexOf(".com") != userMail.value.length - 4){
+//         userMail.style.border = `solid 2px red`;
+//         document.body.innerHTML += `<p style="color:red">Email must end with ".com"</p>`;
+//         return false;
+//     }
+//     return true;
+// }
 
-function validateAge(){
-    var currentYear = new Date().getFullYear();
-    var userYear = +userAge.value.substring(0,4);
-    if(currentYear - userYear < 18){
-        userAge.style.border = `solid 2px red`;
-        document.body.innerHTML += `<p style="color:red">Age must be over 18</p>`;
+// function validateAge(){
+//     var currentYear = new Date().getFullYear();
+//     var userYear = +userAge.value.substring(0,4);
+//     if(currentYear - userYear < 18){
+//         userAge.style.border = `solid 2px red`;
+//         document.body.innerHTML += `<p style="color:red">Age must be over 18</p>`;
+//         return false;
+//     }
+//     return true;
+// }
+// function validatePass(){
+//     if(userPassword.value != userConfirm.value){
+//         userPassword.style.border = `solid 2px red`;
+//         userConfirm.style.border = `solid 2px red`;
+//         document.body.innerHTML += `<p style="color:red">Password are not the same</p>`;
+//         return false;
+//     }
+//     return true;
+// }
+// function validateForm(){
+//     validateUsername();
+//     validateEmail();
+//     validateAge();
+//     validatePass();
+// }
+
+//Another solution
+function validate(){
+    var isLower10Char = userName.value.length < 10;
+    var containCom = userMail.value.lastIndexOf(".com") == userMail.value.length - 4;
+    var isOver18 = new Date().getFullYear() - userAge.value.substring(0,4) >= 18;
+    var arePassSame = userPassword.value == userConfirm.value;
+    if(isLower10Char && containCom && isOver18 && arePassSame){
+        return true;
+    }else{
+        if(!isLower10Char){
+           userName.style.border = `dotted 2px red`;
+           nameLbl.innerHTML += `<p style="color:red">The username must be shorter than 10 characters</p>`;
+        }
+        if(!containCom){
+            userMail.style.border = `dotted 2px red`;
+            mailLbl.innerHTML += `<p style="color:red">Email must end with ".com"</p>`;
+        }
+        if(!isOver18){
+            userAge.style.border = `dotted 2px red`;
+            ageLbl.innerHTML += `<p style="color:red">User must be over 18</p>`;
+        }
+        if(!arePassSame){
+            userPassword.style.border = `solid 2px red`;
+            userConfirm.style.border = `solid 2px red`;
+            passLbl.innerHTML += `<p style="color:red">Password are not the same</p>`;
+            confirmLbl.innerHTML += `<p style="color:red">Password are not the same</p>`;
+        }
         return false;
     }
-    return true;
-}
-function validatePass(){
-    if(userPassword.value != userConfirm.value){
-        userPassword.style.border = `solid 2px red`;
-        userConfirm.style.border = `solid 2px red`;
-        document.body.innerHTML += `<p style="color:red">Password are not the same</p>`;
-        return false;
-    }
-    return true;
-}
-function validateForm(){
-    validateUsername();
-    validateEmail();
-    validateAge();
-    validatePass();
 }
