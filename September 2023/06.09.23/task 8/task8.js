ddc0335947738510dcc9e168d53f0ea4f243faee
nirglus <nirgluskin@gmail.com>
2023-09-06 16:55:40 +0300

    add if statements

--- September 2023/06.09.23/task 8/task8.js
+++ September 2023/06.09.23/task 8/task8.js
@@ -10,6 +10,15 @@ function validateForm(){
     if(isStartUppercase && isBeneath20Char && isOver16Under65 && isEndOnComIl && validatePhone){
         return true;
     }else{
+        if(!isStartUppercase){
+            firstName.style.border = "dotted 2px red";
+            fNameLbl.innerHTML += `<p style="color:red">First name must start with an uppercase letter</p>`
+        }
+        if(!isBeneath20Char){
+            lastName.style.border = "dotted 2px red";
+            lNameLbl.innerHTML += `<p style="color:red">Last name must be under 20 characters</p>`
+        
+        }
         return false;
     }
 }
