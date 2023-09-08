09ae2b475d5c3e9683f39cac021976bbeb5676ec
nirglus <nirgluskin@gmail.com>
2023-09-08 10:37:47 +0300

    add style to body and loginDiv

--- September 2023/07.09.23/todo-list/index.js
+++ September 2023/07.09.23/todo-list/index.js
@@ -1,7 +1,9 @@
-document.body.innerHTML = `
+document.body.innerHTML = `<div id="loginDiv">
+<h1>Please log in</h1>
 <input type="text" id="userName" placeholder="Enter username:">
 <input type="date" id="userAge" placeholder="Date of birth:">
-<button id="loginBtn">Login</button>`
+<button id="loginBtn">Login</button>
+</div>`
 
 loginBtn.addEventListener("click", function(){
     document.body.innerHTML = `<h1>Welcome back <span id="nameSpan">${userName.value}!</span></h1>`
