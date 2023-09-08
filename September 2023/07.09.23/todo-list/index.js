efe2209ac1847372b009050e6f330f458a1ad0fd
nirglus <nirgluskin@gmail.com>
2023-09-08 10:25:25 +0300

    add loginBtn event

--- September 2023/07.09.23/todo-list/index.js
+++ September 2023/07.09.23/todo-list/index.js
@@ -2,3 +2,7 @@ document.body.innerHTML = `
 <input type="text" id="userName" placeholder="Enter username:">
 <input type="date" id="userAge" placeholder="Date of birth:">
 <button id="loginBtn">Login</button>`
+
+loginBtn.addEventListener("click", function(){
+    document.body.innerHTML = `<h1>Welcome back <span id="nameSpan">${userName.value}!</span></h1>`
+})
