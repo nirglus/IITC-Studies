b9e5931c384c86b532e3e77f49076c472484a6c5
nirglus <nirgluskin@gmail.com>
2023-09-08 10:15:02 +0300

    add inputs

---
+++ September 2023/07.09.23/todo-list/index.js
@@ -0,0 +1,4 @@
+document.body.innerHTML = `
+<input type="text" id="userName" placeholder="Enter username:">
+<input type="date" id="userAge" placeholder="Date of birth:">
+<button id="loginBtn">Login</button>`
