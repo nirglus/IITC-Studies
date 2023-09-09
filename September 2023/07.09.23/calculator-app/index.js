8ecbc8f6e67e63a527c1beded9bff44109a1c182
nirglus <nirgluskin@gmail.com>
2023-09-09 12:42:17 +0300

    add html for btns and input

---
+++ September 2023/07.09.23/calculator-app/index.js
@@ -0,0 +1,22 @@
+document.body.innerHTML = `
+<div id="calculator">
+<div id="screen"><input type="text" id="screenInput"></div>
+<div id="numberBtns">
+    <button class="btn" id="1">1</button>
+    <button class="btn" id="2">2</button>
+    <button class="btn" id="3">3</button>
+    <button class="btn" id="4">4</button>
+    <button class="btn" id="5">5</button>
+    <button class="btn" id="6">6</button>
+    <button class="btn" id="7">7</button>
+    <button class="btn" id="8">8</button>
+    <button class="btn" id="9">9</button>
+    <button class="btn" id="0">0</button>
+    <button class="btn" id="dot">.</button>
+    <button class="btn" id="plus">+</button>
+    <button class="btn" id="minus">-</button>
+    <button class="btn" id="multiply">*</button>
+    <button class="btn" id="divide">/</button>
+    <button class="btn" id="even">=</button>
+</div>
+</div>`
