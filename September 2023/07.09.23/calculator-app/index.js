675508633a48e01b2dc2dde536825d0587ba9034
nirglus <nirgluskin@gmail.com>
2023-09-09 13:42:27 +0300

    divide func

--- September 2023/07.09.23/calculator-app/index.js
+++ September 2023/07.09.23/calculator-app/index.js
@@ -46,5 +46,12 @@ function subtract(num1, num2){
 function multiply(num1, num2){
     return num1 * num2;
 }
+function divide(num1, num2){
+    if(num2 == 0){
+        screenInput.value = "CAN NOT DIVIDE BY 0";
+    }else{
+        return num1 / num2;
+    }
+}
 buttonListeners();
 resetBtn();
