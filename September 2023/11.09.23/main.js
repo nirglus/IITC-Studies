fa9d30bfab5a2642bcdd1249415622ceada7a073
nirglus <nirgluskin@gmail.com>
2023-09-11 16:19:48 +0300

    complete task 3

--- September 2023/11.09.23/main.js
+++ September 2023/11.09.23/main.js
@@ -9,7 +9,14 @@ sortArr(myArr);
 //Task 2
 const numArr = [6,7,4,2,8,9];
 function sortNumArr(numArr){
-    const sortedArr = numArr.sort((a , b) => a - b)
+    const sortedArr = numArr.sort((a , b) => a - b);
     console.log(sortedArr);
 }
 sortNumArr(numArr);
+
+//Task 3
+function sortBigToSmall(numArr){
+    const sortedArr = numArr.sort((a , b) => b - a);
+    console.log(sortedArr);
+}
+sortBigToSmall(numArr);
