4e1ff0e1390b222944e6a5b16d1e9c0df91d35a2
nirglus <nirgluskin@gmail.com>
2023-09-12 19:28:38 +0300

    complete task 4

--- September 2023/11.09.23/main.js
+++ September 2023/11.09.23/main.js
@@ -1,22 +1,49 @@
-//Task 1
-const myArr = ["Main","Dom","React","Ajax"];
-function sortArr(myArr){
-    const sortedArr = myArr.sort();
-    console.log(sortedArr);
-}
-sortArr(myArr);
+// //Task 1
+// const myArr = ["Main","Dom","React","Ajax"];
+// function sortArr(myArr){
+//     const sortedArr = myArr.sort();
+//     console.log(sortedArr);
+// }
+// sortArr(myArr);
 
-//Task 2
-const numArr = [6,7,4,2,8,9];
-function sortNumArr(numArr){
-    const sortedArr = numArr.sort((a , b) => a - b);
-    console.log(sortedArr);
-}
-sortNumArr(numArr);
+// //Task 2
+// const numArr = [6,7,4,2,8,9];
+// function sortNumArr(numArr){
+//     const sortedArr = numArr.sort((a , b) => a - b);
+//     console.log(sortedArr);
+// }
+// sortNumArr(numArr);
 
-//Task 3
-function sortBigToSmall(numArr){
-    const sortedArr = numArr.sort((a , b) => b - a);
-    console.log(sortedArr);
+// //Task 3
+// function sortBigToSmall(numArr){
+//     const sortedArr = numArr.sort((a , b) => b - a);
+//     console.log(sortedArr);
+// }
+// sortBigToSmall(numArr);
+
+//Task 4
+const CLASS_ARR = [];
+let counter = 0;
+document.body.innerHTML += `<input id="nameInput" type="text">
+<button id="nextBtn">Next</button>`
+function inputPush(){
+    nextBtn.addEventListener("click", function(){
+        if(counter < 10){
+            CLASS_ARR.push(nameInput.value);
+            console.log(CLASS_ARR);
+            counter++;
+            nameInput.value = "";
+        }
+        else if(counter >= 10){
+                sortClass();
+                return;
+            }
+        }
+    )
 }
-sortBigToSmall(numArr);
+function sortClass(){
+    const sortedClass = CLASS_ARR.sort();
+    console.log(sortedClass);
+}
+
+inputPush();
