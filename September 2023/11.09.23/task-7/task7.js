5538e3a47cba13cc6fc05bcfcecd3c2d4508231a
nirglus <nirgluskin@gmail.com>
2023-09-12 20:22:39 +0300

    complete task 7.1

---
+++ September 2023/11.09.23/task-7/task7.js
@@ -0,0 +1,3 @@
+const COMPANY_ARR = ["Mobileye", "Nvidia", "Fiverr", "Israeli Aerospace Industries",
+"Intel", "PayPal", "Playtika", "Unity" ,"Wix", "Via"];
+COMPANY_ARR.forEach((element) => document.body.innerHTML += `<p>${element}</p>`);
