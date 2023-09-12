b6d3099542fdd08c6ec248f7944fcab214790288
nirglus <nirgluskin@gmail.com>
2023-09-12 20:03:54 +0300

    complete task 6

--- September 2023/11.09.23/main.js
+++ September 2023/11.09.23/main.js
@@ -21,42 +21,49 @@
 // }
 // sortBigToSmall(numArr);
 
-// //Task 4
-// const CLASS_ARR = [];
-// let counter = 0;
-// document.body.innerHTML += `<input id="nameInput" type="text">
-// <button id="nextBtn">Next</button>`
-// function inputPush(){
-//     nextBtn.addEventListener("click", function(){
-//         if(counter < 10){
-//             CLASS_ARR.push(nameInput.value);
-//             console.log(CLASS_ARR);
-//             counter++;
-//             nameInput.value = "";
-//         }
-//         else if(counter >= 10){
-//                 sortClass();
-//                 return;
-//             }
-//         }
-//     )
-// }
-// function sortClass(){
-//     const sortedClass = CLASS_ARR.sort();
-//     console.log(sortedClass);
-// }
-
-// inputPush();
-
-//Task 5
-function randomArr(){
-    const RANDOM_ARR = [];
-    for(let i = 0; i < 10; i++){
-        let randomNum = Math.floor(Math.random()*101);
-        RANDOM_ARR.push(randomNum);
-    }
-    console.log(RANDOM_ARR);
-    const SORTED_ARR = RANDOM_ARR.sort((a,b) => b - a);
-    console.log(SORTED_ARR);
+//Task 4
+const CLASS_ARR = [];
+let counter = 0;
+document.body.innerHTML += `<input id="nameInput" type="text">
+<button id="nextBtn">Next</button>`
+function inputPush(){
+    nextBtn.addEventListener("click", function(){
+        if(counter < 10){
+            CLASS_ARR.push(nameInput.value);
+            console.log(CLASS_ARR);
+            counter++;
+            nameInput.value = "";
+        }
+        else if(counter >= 10){
+                sortClass();
+                return;
+            }
+        }
+    )
 }
-randomArr();
+function sortClass(){
+    const sortedClass = CLASS_ARR.sort();
+    console.log(sortedClass);
+    arrDivide(sortedClass)
+}
+
+inputPush();
+
+// //Task 5
+// function randomArr(){
+//     const RANDOM_ARR = [];
+//     for(let i = 0; i < 10; i++){
+//         let randomNum = Math.floor(Math.random()*101);
+//         RANDOM_ARR.push(randomNum);
+//     }
+//     console.log(RANDOM_ARR);
+//     const SORTED_ARR = RANDOM_ARR.sort((a,b) => b - a);
+//     console.log(SORTED_ARR);
+// }
+// randomArr();
+
+//Task 6
+function arrDivide(sortedClass){
+    const NEW_CLASS_ARR = sortedClass.join("-");
+    console.log(NEW_CLASS_ARR);
+}
