649244aa5133f515975121e877f6bf01992767ca
nirglus <nirgluskin@gmail.com>
2023-09-13 11:07:34 +0300

    complete task 2

--- September 2023/13.09.23/switch-case.js
+++ September 2023/13.09.23/switch-case.js
@@ -1,18 +1,39 @@
-function checkInput(userNum){
-    let userNum = +prompt("Enter a number");
-    switch (userNum) {
-        case 3:
-            console.log("Shalom");
+// //Task 1
+// function checkInput(userNum){
+//     let userNum = +prompt("Enter a number");
+//     switch (userNum) {
+//         case 3:
+//             console.log("Shalom");
+//             return;
+//         case 5:
+//             console.log("Bye bye");
+//             return;
+//         case 7: 
+//         console.log("Thank you");
+//             return;        
+//         default:
+//             console.log("There isn't a proper message");
+//             return;
+//     }
+// }
+// checkInput(userNum);
+
+//Task 2
+function checkName(){
+    let userName = prompt("Enter a name");
+    switch (userName) {
+        case "jacob":
+            document.body.innerHTML = `${userName}`;   
             return;
-        case 5:
-            console.log("Bye bye");
+        case "Nathan":
+            document.body.innerHTML = `${userName.toUpperCase()}`;   
             return;
-        case 7: 
-        console.log("Thank you");
-            return;        
-        default:
-            console.log("There isn't a proper message");
+        case "DALYA":
+            document.body.innerHTML = `${userName.toLowerCase()}`;   
             return;
+        default: 
+        document.body.innerHTML = "There isn't a proper name";
+           return;
     }
 }
-checkInput(userNum);
+checkName();
