b856282e600215242fd34147c0017bb9781f437b
nirglus <nirgluskin@gmail.com>
2023-09-13 15:04:16 +0300

    complete task 8

--- September 2023/13.09.23/loops/loops.js
+++ September 2023/13.09.23/loops/loops.js
@@ -1,39 +1,55 @@
-//Task 1
-const NAME_ARR = ["Dani", "Jacob", "Yuval", "David", "Sunny"];
-// NAME_ARR.forEach((element) =>{
-//     console.log(element);
-// });
+// //Task 1
+// const NAME_ARR = ["Dani", "Jacob", "Yuval", "David", "Sunny"];
+// // NAME_ARR.forEach((element) =>{
+// //     console.log(element);
+// // });
 
-// //Task 2
-const AGE_ARR = [10 , 27 , 35, 8, 5, 45, 69, 9, 2, 90];
-// AGE_ARR.forEach((element) =>{
-//     if(element > 25){
-//         console.log(element);
+// // //Task 2
+// const AGE_ARR = [10 , 27 , 35, 8, 5, 45, 69, 9, 2, 90];
+// // AGE_ARR.forEach((element) =>{
+// //     if(element > 25){
+// //         console.log(element);
+// //     }
+// // })
+
+// // //Task 3
+// const LAST_NAME_ARR = ["Gluskin", "Bengaev", "Fittousi", "AlAluf", "Hazan"];
+// const UPPERCASE_LNAME_ARR = [];
+// // LAST_NAME_ARR.forEach((element) =>{
+// //     UPPERCASE_LNAME_ARR.push(element.toUpperCase());
+// // })
+// // console.log(UPPERCASE_LNAME_ARR);
+
+// //Task 4
+// for(const name of NAME_ARR){
+//     console.log(name);
+// }
+
+// //Task 5
+// for(const age of AGE_ARR){
+//     if(age > 25){
+//         console.log(age);
 //     }
-// })
+// }
 
-// //Task 3
-const LAST_NAME_ARR = ["Gluskin", "Bengaev", "Fittousi", "AlAluf", "Hazan"];
-const UPPERCASE_LNAME_ARR = [];
-// LAST_NAME_ARR.forEach((element) =>{
-//     UPPERCASE_LNAME_ARR.push(element.toUpperCase());
-// })
+// //Task 6
+// for(const lName of LAST_NAME_ARR){
+//     UPPERCASE_LNAME_ARR.push(lName.toUpperCase());
+// }
 // console.log(UPPERCASE_LNAME_ARR);
 
-//Task 4
-for(const name of NAME_ARR){
-    console.log(name);
+//Task 8
+const HOUSE_OBJ = {
+    street: "Hertzel",
+    city: "Lod",
+    country: "Israel",
+    rooms: "4",
+    isResidents: true 
 }
-
-//Task 5
-for(const age of AGE_ARR){
-    if(age > 25){
-        console.log(age);
-    }
+console.log(HOUSE_OBJ);
+for(const houseKey in HOUSE_OBJ){
+    console.log(houseKey);
 }
-
-//Task 6
-for(const lName of LAST_NAME_ARR){
-    UPPERCASE_LNAME_ARR.push(lName.toUpperCase());
+for(const houseValue in HOUSE_OBJ){
+    console.log(HOUSE_OBJ[houseValue]);
 }
-console.log(UPPERCASE_LNAME_ARR);
