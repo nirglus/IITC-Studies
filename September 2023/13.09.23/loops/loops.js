330c22b717ae9aeb81d9fdadf9e4b01324b15d45
nirglus <nirgluskin@gmail.com>
2023-09-13 13:49:28 +0300

    complete task 4

--- September 2023/13.09.23/loops/loops.js
+++ September 2023/13.09.23/loops/loops.js
@@ -1,21 +1,26 @@
 //Task 1
 const NAME_ARR = ["Dani", "Jacob", "Yuval", "David", "Sunny"];
-NAME_ARR.forEach((element) =>{
-    console.log(element);
-});
+// NAME_ARR.forEach((element) =>{
+//     console.log(element);
+// });
 
-//Task 2
-const AGE_ARR = [10 , 27 , 35, 8, 5, 45, 69, 9, 2, 90];
-AGE_ARR.forEach((element) =>{
-    if(element > 25){
-        console.log(element);
-    }
-})
+// //Task 2
+// const AGE_ARR = [10 , 27 , 35, 8, 5, 45, 69, 9, 2, 90];
+// AGE_ARR.forEach((element) =>{
+//     if(element > 25){
+//         console.log(element);
+//     }
+// })
 
-//Task 3
-const LAST_NAME_ARR = ["Gluskin", "Bengaev", "Fittousi", "AlAluf", "Hazan"];
-const UPPERCASE_LNAME_ARR = [];
-LAST_NAME_ARR.forEach((element) =>{
-    UPPERCASE_LNAME_ARR.push(element.toUpperCase());
-})
-console.log(UPPERCASE_LNAME_ARR);
+// //Task 3
+// const LAST_NAME_ARR = ["Gluskin", "Bengaev", "Fittousi", "AlAluf", "Hazan"];
+// const UPPERCASE_LNAME_ARR = [];
+// LAST_NAME_ARR.forEach((element) =>{
+//     UPPERCASE_LNAME_ARR.push(element.toUpperCase());
+// })
+// console.log(UPPERCASE_LNAME_ARR);
+
+//Task 4
+for(const name of NAME_ARR){
+    console.log(name);
+}
