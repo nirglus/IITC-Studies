3a12038893d14c69b81b4205a6ae449b0154342b
nirglus <nirgluskin@gmail.com>
2023-09-13 16:57:49 +0300

    complete task 4

--- September 2023/13.09.23/switch-case/switch-case.js
+++ September 2023/13.09.23/switch-case/switch-case.js
@@ -17,43 +17,54 @@
 // }
 // checkInput(userNum);
 
-//Task 2
-function checkName(){
-    let userName = prompt("Enter a name");
-    switch (userName) {
-        case "jacob":
-            document.body.innerHTML = `${userName}`;   
-            return;
-        case "Nathan":
-            document.body.innerHTML = `${userName.toUpperCase()}`;   
-            return;
-        case "DALYA":
-            document.body.innerHTML = `${userName.toLowerCase()}`;   
-            return;
-        default: 
-            document.body.innerHTML = "There isn't a proper name";
-    }
-}
-checkName();
+// //Task 2
+// function checkName(){
+//     let userName = prompt("Enter a name");
+//     switch (userName) {
+//         case "jacob":
+//             document.body.innerHTML = `${userName}`;   
+//             return;
+//         case "Nathan":
+//             document.body.innerHTML = `${userName.toUpperCase()}`;   
+//             return;
+//         case "DALYA":
+//             document.body.innerHTML = `${userName.toLowerCase()}`;   
+//             return;
+//         default: 
+//             document.body.innerHTML = "There isn't a proper name";
+//     }
+// }
+// checkName();
 
-//Task 3
-function calculate(){
-    let num1 = +prompt("Enter first number");
-    let num2 = +prompt("Enter second number");
-    let param = prompt("Enter a paramater (+ - * /)");
-    switch (param) {
-        case "+":
-            console.log(num1 + num2);
-            return;
-        case "-":
-            console.log(num2 - num1);
-            return;
-        case "/":
-            console.log(num2 / num1);
-            return;
-        case "*":
-            console.log(num1 * num2);
-            return;    
-        default:
-    }
+// //Task 3
+// function calculate(){
+//     let num1 = +prompt("Enter first number");
+//     let num2 = +prompt("Enter second number");
+//     let param = prompt("Enter a paramater (+ - * /)");
+//     switch (param) {
+//         case "+":
+//             console.log(num1 + num2);
+//             return;
+//         case "-":
+//             console.log(num2 - num1);
+//             return;
+//         case "/":
+//             console.log(num2 / num1);
+//             return;
+//         case "*":
+//             console.log(num1 * num2);
+//             return;    
+//         default:
+//     }
+// }
+
+//Task 4
+function printText(){
+    let userTxt = prompt("Enter a text");
+    let userColor = prompt("Enter color");
+    let userFontSize = +prompt("Enter font size");
+    document.body.innerHTML += `<p id="printedTxt">${userTxt}</p>`;
+    printedTxt.style.color = `${userColor}`;
+    printedTxt.style.fontSize = `${userFontSize}px`;
 }
+printText();
