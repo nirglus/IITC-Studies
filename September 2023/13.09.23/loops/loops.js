026d8388f27c2922731b9ed10f4312ebeef79541
nirglus <nirgluskin@gmail.com>
2023-09-13 12:00:49 +0300

    complete task 3

--- September 2023/13.09.23/loops/loops.js
+++ September 2023/13.09.23/loops/loops.js
@@ -11,3 +11,11 @@ AGE_ARR.forEach(function(element){
         console.log(element);
     }
 })
+
+//Task 3
+const LAST_NAME_ARR = ["Gluskin", "Bengaev", "Fittousi", "AlAluf", "Hazan"];
+const UPPERCASE_LNAME_ARR = [];
+LAST_NAME_ARR.forEach(function(element){
+    UPPERCASE_LNAME_ARR.push(element.toUpperCase());
+})
+console.log(UPPERCASE_LNAME_ARR);
