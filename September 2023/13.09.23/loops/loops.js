1fd7de34b46b6dfe09caa377ef38877612306fb3
nirglus <nirgluskin@gmail.com>
2023-09-13 11:57:31 +0300

    complete task 2

--- September 2023/13.09.23/loops/loops.js
+++ September 2023/13.09.23/loops/loops.js
@@ -2,4 +2,12 @@
 const NAME_ARR = ["Dani", "Jacob", "Yuval", "David", "Sunny"];
 NAME_ARR.forEach(function(element){
     console.log(element);
+});
+
+//Task 2
+const AGE_ARR = [10 , 27 , 35, 8, 5, 45, 69, 9, 2, 90];
+AGE_ARR.forEach(function(element){
+    if(element > 25){
+        console.log(element);
+    }
 })
