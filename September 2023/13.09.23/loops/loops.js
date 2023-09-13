333c0f6b08864eb9f481776df0d1a3b9d9f7e2fe
nirglus <nirgluskin@gmail.com>
2023-09-13 12:02:08 +0300

    switch to arrow functions

--- September 2023/13.09.23/loops/loops.js
+++ September 2023/13.09.23/loops/loops.js
@@ -1,12 +1,12 @@
 //Task 1
 const NAME_ARR = ["Dani", "Jacob", "Yuval", "David", "Sunny"];
-NAME_ARR.forEach(function(element){
+NAME_ARR.forEach((element) =>{
     console.log(element);
 });
 
 //Task 2
 const AGE_ARR = [10 , 27 , 35, 8, 5, 45, 69, 9, 2, 90];
-AGE_ARR.forEach(function(element){
+AGE_ARR.forEach((element) =>{
     if(element > 25){
         console.log(element);
     }
@@ -15,7 +15,7 @@ AGE_ARR.forEach(function(element){
 //Task 3
 const LAST_NAME_ARR = ["Gluskin", "Bengaev", "Fittousi", "AlAluf", "Hazan"];
 const UPPERCASE_LNAME_ARR = [];
-LAST_NAME_ARR.forEach(function(element){
+LAST_NAME_ARR.forEach((element) =>{
     UPPERCASE_LNAME_ARR.push(element.toUpperCase());
 })
 console.log(UPPERCASE_LNAME_ARR);
