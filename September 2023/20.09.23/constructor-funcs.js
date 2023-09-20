87971c0d05ffda614279d4340ca97227a43e093e
nirglus <nirgluskin@gmail.com>
2023-09-20 11:39:11 +0300

    add style to book item, change render func

--- September 2023/20.09.23/constructor-funcs.js
+++ September 2023/20.09.23/constructor-funcs.js
@@ -33,19 +33,29 @@ console.log(car1.drive());
 
 //Class Task 1
 
-function Book(title, category, author, pages){
+function Book(title, category, author, pages, image){
     this.title = title;
     this.category = category;
     this.author = author;
     this.pages = pages;
+    this.image = image;
     this.render = function render(){
-        return null;
+        return `<div class="bookItem">
+        <h1>${this.title}</h1>
+        <p><b>Category:</b> ${this.category}</p>
+        <p><b>Author:</b> ${this.author}</p>
+        <p><b>Pages:</b> ${this.pages}</p>
+        <img src="${this.image}">`;
     }
 }
 
-const book1 = new Book("The Godfather", "Crime", "Mario Puzo", 433);
-const book2 = new Book("Harry Potter", "Fantasy", "J.K Rolling", 766);
-const book3 = new Book("The Godfather Returns", "Crime", "Mark Winegardner", 448)
+const book1 = new Book("The Godfather", "Crime", "Mario Puzo", 433, "https://m.media-amazon.com/images/I/81IHPwG1tbL._SY522_.jpg");
+const book2 = new Book("Harry Potter", "Fantasy", "J.K Rolling", 766, "https://m.media-amazon.com/images/I/71-++hbbERL._AC_SY300_SX300_.jpg");
+const book3 = new Book("The Godfather Returns", "Crime", "Mark Winegardner", 448, "https://m.media-amazon.com/images/I/51UYngmZ-2L._SY445_SX342_.jpg")
 
 const BOOKS_ARR = [book1, book2, book3];
-console.log(BOOKS_ARR);
+console.log(BOOKS_ARR[0].render());
+
+BOOKS_ARR.forEach((book) => {
+    document.getElementById("book").innerHTML += book.render();
+})
