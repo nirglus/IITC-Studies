d5f3b2c419da85adedf2255399010a631b03d0e3
nirglus <nirgluskin@gmail.com>
2023-09-27 11:17:05 +0300

    fetch format

--- September 2023/27.09.23/promises.js
+++ September 2023/27.09.23/promises.js
@@ -1,6 +1,11 @@
+// Fetch format
 fetch("https://jsonplaceholder.typicode.com/todos")
-.then(response => { //gets an array from the URL
+    .then(response => { //gets an array from the URL
         console.log(response);
+        return response.json(); // passes the response.json to the next .then func
+    })
+    .then(data => {
+        console.log(data);
     })
     .catch(error => {
         console.log(error);
