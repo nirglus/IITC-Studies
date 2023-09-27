54e2f95226205ead7c648829c7d0ba1518c61360
nirglus <nirgluskin@gmail.com>
2023-09-27 12:06:43 +0300

    add exercise

--- September 2023/27.09.23/promises.js
+++ September 2023/27.09.23/promises.js
@@ -10,3 +10,56 @@ fetch("https://jsonplaceholder.typicode.com/todos")
     .catch(error => {
         console.log(error);
     })
+
+// Fetch Exercise
+
+fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res => {
+        console.log(res);
+        return res.json();
+    })
+    // .then(data => {
+    //     console.log(data);
+    //     for(const obj of data){
+    //         for(const key in obj){
+    //             document.body.innerHTML += `<p>${key} : ${obj[key]}</p>`;
+    //         }
+    //     }
+    // })
+    .then(data =>{
+        console.log(data);
+    })
+    .catch(err => {
+        console.log(err);
+    })
+
+
+fetch("https://jsonplaceholder.typicode.com/photos")
+    .then(res => {
+        console.log(res);
+        return res.json();
+    })
+    .then(data => {
+        console.log(data);
+    })
+    .catch(err => {
+        console.log(err);
+    })
+
+    
+fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(response =>{
+        console.log(response);
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+        for(const obj of data){
+            for(const key in obj){
+                document.body.innerHTML += `<p>${key} : ${obj[key]}</p>`;
+            }
+        }
+    })
+    .catch(error =>{
+        console.log(error);
+    })
