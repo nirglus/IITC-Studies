616376f510f383e7aeee6ff4ab049c43984a4bc8
nirglus <nirgluskin@gmail.com>
2024-01-27 20:56:45 +0200

    adjust navbar

--- 2024/January 2024/14.01.24/expans-react/src/components/Navbar/index.jsx
+++ 2024/January 2024/14.01.24/expans-react/src/components/Navbar/index.jsx
@@ -1,13 +1,20 @@
 import { Link } from "react-router-dom"
-
+import logo from "../../assets/budgetBuddyLogo.png";
+import "./Navbar.css";
 function Navbar({user, userSignOut}){
     return(
     <nav>
+        <div className="logo">
+            <img src={logo} alt="budgetBuddyLogo" />
+            <h1>BudgetBuddy</h1>
+        </div>
+        <div className="navItems">
             <Link to="/" style={{padding: "5px"}}>Home</Link>
             <Link to="/about" style={{padding: "5px"}}>About</Link>
             <Link to="/budget" style={{padding: "5px"}}>Budget</Link>
             <Link to ="/login" style={{padding: "5px"}}>Login</Link>
             {user ? <button onClick={userSignOut}>Sign out</button> : null}
+        </div>
     </nav>
     )
 }
