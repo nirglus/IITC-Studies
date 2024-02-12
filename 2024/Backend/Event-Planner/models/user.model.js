96c4b39d512ccfa74d867f9cf93c6c5aed33804f
nirglus <nirgluskin@gmail.com>
2024-02-12 10:45:29 +0200

    add patch for user

--- 2024/Backend/Event-Planner/models/user.model.js
+++ 2024/Backend/Event-Planner/models/user.model.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    email: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
-    fullName: {type: String, required: true}
+    fullName: {type: String, required: true},
+    id: {type: String, required: false}
 });
 
 const User = mongoose.model("User", userSchema);
--- 2024/Backend/Event-Planner/routes/users.routes.js
+++ 2024/Backend/Event-Planner/routes/users.routes.js
@@ -6,6 +6,7 @@ router.post("/register", async (req, res) =>{
     const body = req.body;
     try {
         const newUser = new User(body);
+        newUser.id = newUser._id;
         await newUser.save();
         res.send({message: "User saved successfully!", data: newUser});
     } catch (error) {
@@ -22,6 +23,17 @@ router.post("/login", async (req, res) =>{
         } else{
             res.send({message: "Wrong email or password", data: null});
         }
+    } catch (error) {
+        res.status(400).send("Error");
+    }
+});
+
+router.patch("/:id", async (req, res)=>{
+    const { id } = req.params;
+    const body = req.body;
+    try {
+        const user = await User.findByIdAndUpdate(id, body, {new: true});
+        return res.send(user);
     } catch (error) {
         res.status(400).send("Error");
     }
