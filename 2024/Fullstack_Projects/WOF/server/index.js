c7e8ccfd775e5f5f0a030c9f0111585d5afe073f
nirglus <nirgluskin@gmail.com>
2024-02-18 09:04:29 +0200

    connect mongoose

--- 2024/Fullstack_Projects/WOF/server/index.js
+++ 2024/Fullstack_Projects/WOF/server/index.js
@@ -1,4 +1,15 @@
-const { app } = require("./app")
+const { app } = require("./app");
+const mongoose = require("mongoose");
+
+const mongoUrl = 'mongodb://localhost:27017/wof';
+
+mongoose.connect(mongoUrl)
+   .then(() =>{
+    console.log("Connected to db");
+   }).catch(error =>{
+    console.log(error);
+   });
+
 const PORT = process.env.PORT || 2000;
 
 app.listen(PORT, () =>{
