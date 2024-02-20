4c3554e923490d3c6cc7ad4dc846b62036e1fafe
nirglus <nirgluskin@gmail.com>
2024-02-20 21:00:12 +0200

    add orders route

---
+++ 2024/Fullstack_Projects/WOF/frontend/src/context/User.jsx
@@ -0,0 +1,7 @@
+import { useState, useEffect, createContext} from "react";
+import { axios } from "axios";
+
+export const UserContext = createContext({});
+export default function UserProvider(){
+    
+}
--- 2024/Fullstack_Projects/WOF/server/app.js
+++ 2024/Fullstack_Projects/WOF/server/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const usersRouter = require("./routes/users.routes");
 const productsRouter = require("./routes/products.routes");
 const cartsRouter = require("./routes/carts.routes");
+const ordersRouter = require("./routes/orders.routes");
 const cors = require("cors");
 
 const app = express();
@@ -11,6 +12,7 @@ app.use(cors());
 app.use("/wof/users", usersRouter);
 app.use("/wof/products",productsRouter);
 app.use("/wof/cart", cartsRouter);
+app.use("/wof/orders", ordersRouter);
 
 
 module.exports = { app }
