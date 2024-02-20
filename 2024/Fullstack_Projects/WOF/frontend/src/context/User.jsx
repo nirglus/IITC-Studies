4a14b7b4566bd4c833fef116c1d95bd8270ff824
nirglus <nirgluskin@gmail.com>
2024-02-20 21:08:38 +0200

    add user context

--- 2024/Fullstack_Projects/WOF/frontend/src/context/User.jsx
+++ 2024/Fullstack_Projects/WOF/frontend/src/context/User.jsx
@@ -3,5 +3,23 @@ import { axios } from "axios";
 
 export const UserContext = createContext({});
 export default function UserProvider(){
+    const [user, setUser] = useState();
     
+    useEffect(() => {
+        const fetchUser = async () => {
+            try {
+                const response = await axios.get('http://localhost:2000/users');
+                setUser(response.data); 
+            } catch (error) {
+                console.error('Error fetching user:', error);
+            }
+        };
+
+        fetchUser(); 
+    }, []);
+    return (
+        <UserContext.Provider value={{ user }}>
+            {children}
+        </UserContext.Provider>
+    );
 }
