4417116b5a3533b6adb235423f9e32ce0e051552
nirglus <nirgluskin@gmail.com>
2024-02-24 16:54:46 +0200

    create order functionallity

--- 2024/Fullstack_Projects/WOF/frontend/src/components/Checkout/Checkout.jsx
+++ 2024/Fullstack_Projects/WOF/frontend/src/components/Checkout/Checkout.jsx
@@ -1,5 +1,8 @@
-import {useState} from 'react'
-import CheckoutForm from '../MiniComponents/CheckoutForm/CheckoutForm'
+import {useState, useContext} from 'react'
+import { CartContext } from '../../context/Cart';
+import CheckoutForm from '../MiniComponents/CheckoutForm/CheckoutForm';
+import axios from 'axios';
+import { baseURL } from '../../config/serverConfig';
 
 function Checkout() {
     const [address, setAddress] = useState({
@@ -8,6 +11,7 @@ function Checkout() {
         state: '',
         country: ''
     });
+    const {userCart} = useContext(CartContext);
 
     const handleChange = (e) => {
         setAddress({
@@ -18,6 +22,18 @@ function Checkout() {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        try {
+            const res = await axios.post(`${baseURL}/orders`, {
+                userID: userCart.userID,
+                address: address,
+                cartID: userCart.id,
+                items: userCart.items,
+                totalPrice: userCart.totalPrice
+            });
+            console.log(res.data.message);
+        } catch (error) {
+            console.log(`Could'nt create order`, error);
+        }
     };
   return (
     <div>
