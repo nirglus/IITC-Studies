940ce278703374bc5347b88a301effc3473e748d
nirglus <nirgluskin@gmail.com>
2024-02-27 16:36:39 +0200

    adjust home page

---
+++ 2024/Fullstack_Projects/WOF/frontend/src/helpers/images.js
@@ -0,0 +1,8 @@
+export const homeImages = [
+    'https://www.diecastxmagazine.com/wp-content/uploads/2018/02/lead-614x300.jpg',
+    'https://i.redd.it/bburago-ferrari-f1-75-1-18-i-couldnt-resist-doing-the-v0-49wzm32ebtga1.jpg?width=3877&format=pjpg&auto=webp&s=91f4968022b6ea1856aaefd3a911689a3b7f3f0c',
+    'https://cdn11.bigcommerce.com/s-yrkef1j7/images/stencil/1280x1280/products/35065/154075/QQ20230810055519__94332.1691672253.png?c=2',
+    'https://media.formula1.com/image/upload/content/dam/fom-website/sutton/2019/Azerbaijan/Thursday/1017466901-LAT-20190428-_R3I6639.jpg.transform/9col/image.jpg',
+    'https://giulymodelstore.com/images/stories/virtuemart/product/sn40.jpg'
+
+]
--- 2024/Fullstack_Projects/WOF/frontend/src/pages/Home/Home.jsx
+++ 2024/Fullstack_Projects/WOF/frontend/src/pages/Home/Home.jsx
@@ -1,13 +1,39 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import "./Home.scss";
+import { homeImages } from "../../helpers/images";
 
 function Home() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % homeImages.length);
+    }, 3000);
+  
+    return () => clearInterval(interval);
+  }, [homeImages.length, currentIndex]);
+  
+
   return (
-    <div>
-          <h1>Welcome to World of Formula!</h1>
-          <p>Checkout our formula 1 diecast</p>
-          <button><Link to="/products">Show me the products</Link></button>
+    // <div>
+    //       <h1>Welcome to World of Formula!</h1>
+    //       <p>Checkout our formula 1 diecast</p>
+    //       <button><Link to="/products">Show me the products</Link></button>
+    // </div>
+    <div className="home">
+      <div className="home-container">
+      <div className="image-slider">
+        <img src={homeImages[currentIndex]} alt={`Image ${currentIndex}`} className="current" />
+      </div>
+        <div className="content-overlay">
+          <h1 className="welcome-heading">Welcome to World of Formula!</h1>
+          <p className="thrills-description">Explore the Thrills of Formula 1 Diecast Collectibles</p>
+          <button><Link to="/products">View Products</Link></button>
+        </div>
+      </div>
     </div>
-  )
-}
+    );
+  }
 
 export default Home
