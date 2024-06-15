e9c44a8991b7681939f7d8c508a9db3051fd774c
nirglus <nirgluskin@gmail.com>
2024-06-15 17:56:20 +0300

    add pingtask

--- 2024/Fullstack_Projects/WOF/server/app.js
+++ 2024/Fullstack_Projects/WOF/server/app.js
@@ -3,6 +3,8 @@ const usersRouter = require("./routes/users.routes");
 const productsRouter = require("./routes/products.routes");
 const cartsRouter = require("./routes/carts.routes");
 const ordersRouter = require("./routes/orders.routes");
+const pingRoute = require('./routes/ping.routes');
+
 const cors = require("cors");
 
 const app = express();
@@ -13,6 +15,6 @@ app.use("/wof/users", usersRouter);
 app.use("/wof/products",productsRouter);
 app.use("/wof/cart", cartsRouter);
 app.use("/wof/orders", ordersRouter);
-
+app.use('/wof/ping', pingRoute);
 
 module.exports = { app }
