9f5a5cb9e691fdf19532826ff52722727de6d9ba
nirglus <nirgluskin@gmail.com>
2024-07-03 17:41:48 +0300

    add player display functionallity

--- 2024/Self-Exercise/React-The Complete Guide 2024/Essentials-Deep Dive/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 2024/Self-Exercise/React-The Complete Guide 2024/Essentials-Deep Dive/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,18 +1,37 @@
+import { useState } from "react";
+
 const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
-]
-function GameBoard() {
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+function GameBoard({onSelectSquare, activePlayerSymbol}) {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  const handleSelectSquare = (rowIndex, colIndex) =>{
+    setGameBoard((prevGameBoard) => {
+        const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+        updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+        return updatedBoard;
+    });
+    onSelectSquare();
+  }
+
   return (
     <ol id="game-board">
-       {initialGameBoard.map((row, rowIndex) => <li key={rowIndex}>
-        <ol>
-            {row.map((col, colIndex) => <li key={colIndex}><button>{col}</button></li>)}
-        </ol>
-       </li>)} 
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
     </ol>
-  )
+  );
 }
 
-export default GameBoard
+export default GameBoard;
