95acf015a8e9e7786206fc103fa27bcbfc1ebd30
nirglus <nirgluskin@gmail.com>
2024-07-08 19:05:08 +0300

    add timer ref, timerStarted, timerExpired

---
+++ 2024/Self-Exercise/React-The Complete Guide 2024/Working with Refs & Portals/01-starting-project/src/components/TimerChallenge.jsx
@@ -0,0 +1,36 @@
+import { useRef, useState } from "react";
+
+function TimerChallenge({title, targetTime}) {
+  const timer = useRef();
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+
+  const handleStart = () =>{
+    timer.current = setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+
+    setTimerStarted(true);
+  }
+
+  const handleStop = () =>{
+    clearTimeout(timer.current);
+  }
+  return (
+    <section className="challenge">
+        <h2>{title}</h2>
+        {timerExpired && <p>You lost!</p>}
+        <p className="challenge-time">
+            {targetTime} second{targetTime > 1 ? 's' : ''}
+        </p>
+        <button onClick={timerStarted ? handleStop : handleStart}>
+            {timerStarted ? 'Stop' : 'Start'} Challenge
+        </button>
+        <p className={timerStarted ? 'active' : undefined}>
+            {timerStarted ? 'Timer is running...' : 'Timer inactive'}
+        </p>
+    </section>
+  )
+}
+
+export default TimerChallenge
