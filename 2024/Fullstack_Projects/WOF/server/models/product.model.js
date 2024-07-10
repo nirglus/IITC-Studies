327d33d531ad10e0a94d43e765ca383ab88842ad
nirglus <nirgluskin@gmail.com>
2024-07-10 14:30:26 +0300

    update product model

--- 2024/Fullstack_Projects/WOF/server/models/product.model.js
+++ 2024/Fullstack_Projects/WOF/server/models/product.model.js
@@ -4,6 +4,10 @@ const productSchema = new mongoose.Schema({
     id: {type: String, required: false},
     title: {type: String, required: true},
     image: {type: String, required: true},
+    images: [{
+        url: {type: String, required: false},
+        title: {type: String, required: false}
+    }],
     description: {type: String, required: true},
     scale: {type: String, required: false},
     totalQuantity: {type: Number, required: true},
