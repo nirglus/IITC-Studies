81a8d33f48173d95153fd8e1943792b0099be604
nirglus <nirgluskin@gmail.com>
2024-07-10 10:45:23 +0300

    add modals, add createPortal for modal

--- 2024/Fullstack_Projects/WOF/frontend/src/components/AddToCartModal/AddToCartModal.jsx
+++ 2024/Fullstack_Projects/WOF/frontend/src/components/AddToCartModal/AddToCartModal.jsx
@@ -1,4 +1,5 @@
 import { forwardRef, useRef, useImperativeHandle } from "react";
+import { createPortal } from "react-dom"
 import "./AddToCartModal.scss";
 
 const AddToCartModal = forwardRef(function AddToCartModal({}, ref){
@@ -12,15 +13,15 @@ const AddToCartModal = forwardRef(function AddToCartModal({}, ref){
         }
       })
 
-      return (
+      return createPortal(
         <dialog ref={dialog} className="addItemModal">
             <div className="modalContent">
-                <h2><i class="bi bi-check2-circle"></i> Item added to cart!</h2>
+                <h2><i className="bi bi-check2-circle"></i> Item added to cart!</h2>
                 <form method="dialog">
                     <button className='addToCartBtn'>Continue shopping</button>
                 </form>
             </div>
-        </dialog>
+        </dialog>, document.getElementById('modals')
       )
 })
 
