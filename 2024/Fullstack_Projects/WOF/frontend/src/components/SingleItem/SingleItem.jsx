ed636743234faec7c2bab654207e1fe634baf9b7
nirglus <nirgluskin@gmail.com>
2024-07-10 11:24:41 +0300

    add dialog ref for SingleItem

--- 2024/Fullstack_Projects/WOF/frontend/src/components/SingleItem/SingleItem.jsx
+++ 2024/Fullstack_Projects/WOF/frontend/src/components/SingleItem/SingleItem.jsx
@@ -1,18 +1,20 @@
 import { useParams } from "react-router-dom";
-import { useEffect, useState, useContext } from "react";
+import { useEffect, useState, useContext, useRef } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { baseURL } from "../../config/serverConfig";
 import { CartContext } from '../../context/Cart';
 import placeholderImage from '../../assets/no-image.png';
 import "./SingleItem.scss";
+import AddToCartModal from "../AddToCartModal/AddToCartModal";
 
 function SingleItem() {
   let {id: itemID} = useParams();
   const [item, setItem] = useState({});
   const [quantity, setQuantity] = useState(1); 
   const {addItemToCart, userCart} = useContext(CartContext);
-  const [imgSrc, setImgSrc] = useState(item.image); 
+  const [imgSrc, setImgSrc] = useState(item.image);
+  const dialog = useRef();
 
   const getItem = async(itemID) =>{
       try {
@@ -39,8 +41,8 @@ function SingleItem() {
       console.log({userCart});
       const cartID = userCart.id;
       const price = item.price;
-      console.log({productID, cartID, price, quantity});
       addItemToCart({productID, cartID, price, quantity});
+      dialog.current.open();
     };
 
     const handleImageError = () => {
@@ -59,6 +61,7 @@ function SingleItem() {
 
   return (
       <>
+        <AddToCartModal ref={dialog} />
         <div className="singleProduct">
           <div className="productImages">
             <img className='mainImg' src={imgSrc} alt={item.title} width={600} onError={handleImageError}/>
