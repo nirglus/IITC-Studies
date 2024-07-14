be6b727e62dfa2c7b06723b98c8dbe1852af3b3c
nirglus <nirgluskin@gmail.com>
2024-07-14 13:15:26 +0300

    update view products button

--- 2024/Fullstack_Projects/WOF/frontend/src/pages/Home/Home.jsx
+++ 2024/Fullstack_Projects/WOF/frontend/src/pages/Home/Home.jsx
@@ -6,15 +6,6 @@ import HomeBrands from "../../components/MiniComponents/HomeBrands/HomeBrands";
 import BenefitsSection from "../../components/MiniComponents/BenefitsSection/BenefitsSection";
 
 function Home() {
-  // const [currentIndex, setCurrentIndex] = useState(0);
-
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     setCurrentIndex((prevIndex) => (prevIndex + 1) % homeImages.length);
-  //   }, 3000);
-  
-  //   return () => clearInterval(interval);
-  // }, [homeImages.length, currentIndex]);
   useEffect(() => {
     window.scrollTo(0, 0); 
   }, []);
@@ -23,27 +14,13 @@ function Home() {
 
   return (
     <div className="home">
-      {/* <div className="home-container">
-      <div className="image-slider">
-        <img src={homeImages[currentIndex]} alt={`Image ${currentIndex}`} className="current" />
-      </div>
-        <div className="content-overlay">
-          <h1 className="welcome-heading">Welcome to World of Formula!</h1>
-          <div className="welcome-description">
-            <p>Explore and collect your favorite Formula 1 cars and teams to adorn your collection!</p>
-            <p>Immerse yourself in the world of Formula 1, where passion meets craftsmanship, and relive the excitement of iconic races.</p>
-
-          </div>
-          <button><Link to="/products">View Products</Link></button>
-        </div>
-      </div> */}
       <div className="homeContainer">
         <div className="contentOverlay">
           <h1 className="welcomeHeading">Start your engines</h1>
           <div className="welcomeDescription">
             <p>Explore and collect your favorite Formula 1 cars and teams to adorn your collection!</p>
           </div>
-          <button><Link to="/products">View Products</Link></button>
+          <Link className="viewProducts" to="/products">View Products</Link>
         </div>
       </div>
       <HomeBrands />
