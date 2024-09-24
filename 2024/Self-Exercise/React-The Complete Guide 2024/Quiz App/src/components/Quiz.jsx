1cc4b1df95bba677e46f3866003304db0955a5f0
nirglus <nirgluskin@gmail.com>
2024-09-24 04:57:55 +0300

    add quiz logic, display summary

--- 2024/Self-Exercise/React-The Complete Guide 2024/Quiz App/src/components/Quiz.jsx
+++ 2024/Self-Exercise/React-The Complete Guide 2024/Quiz App/src/components/Quiz.jsx
@@ -1,21 +1,37 @@
 import { useState } from "react";
 import QUESTIONS from "../questions";
+import completeImg from "../assets/quiz-complete.png";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
+
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={completeImg} alt="quiz-complete-img" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
 
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevUserAnswers) => {
       return [...prevUserAnswers, selectedAnswer];
     });
   }
+  
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
