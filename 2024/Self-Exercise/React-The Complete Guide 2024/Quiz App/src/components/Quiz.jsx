82942cb26702bc7bdec756839447dc17836a9000
nirglus <nirgluskin@gmail.com>
2024-09-25 02:21:45 +0300

    add questiontimer

--- 2024/Self-Exercise/React-The Complete Guide 2024/Quiz App/src/components/Quiz.jsx
+++ 2024/Self-Exercise/React-The Complete Guide 2024/Quiz App/src/components/Quiz.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import QUESTIONS from "../questions";
 import completeImg from "../assets/quiz-complete.png";
+import QuestionTimer from "./QuestionTimer";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
@@ -29,6 +30,7 @@ export default function Quiz() {
   return (
     <div id="quiz">
       <div id="question">
+        <QuestionTimer timeout={10000} onTimeout={() => handleSelectAnswer(null)}/>
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
           {shuffledAnswers.map((answer) => (
